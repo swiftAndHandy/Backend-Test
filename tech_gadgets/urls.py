11fc9e8becf05f2aba2cab25765d6bc6bfd96f25
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-10-11 11:14:52 +0200

    changed POST and GET-Functions for cleaner code

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import start_page_view, single_gadget_view, single_gadget_slug_view, single_gadget_post_view
+from .views import start_page_view, single_gadget_int_view, single_gadget_view, GadgetView
 
 urlpatterns = [
     path('', start_page_view),
-    path('gadget/<int:gadget_id>', single_gadget_view),
-    path('gadget/send_gadget/', single_gadget_post_view),
-    path('gadget/<slug:gadget_slug>', single_gadget_slug_view, name="gadget_slug_url"),
+    path('gadget/', GadgetView.as_view()),
+    path('gadget/<int:gadget_id>', single_gadget_int_view),
+    path('gadget/<slug:gadget_slug>', GadgetView.as_view(), name="gadget_slug_url"),
 ]
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, Http404
 from django.utils.text import slugify
 from django.urls import reverse
-from django.views.decorators.csrf import csrf_exempt
+from django.views import View
 import json
 
 from .dummy_data import gadgets
@@ -11,29 +11,30 @@ from .dummy_data import gadgets
 def start_page_view(request):
     return HttpResponse("läuft!")
 
-def single_gadget_view(request, gadget_id):
+def single_gadget_int_view(request, gadget_id):
     if len(gadgets) > gadget_id:
         new_slug = slugify(gadgets[gadget_id]['name'])
         new_url = reverse("gadget_slug_url", args=[new_slug])
         return redirect(new_url)
     return HttpResponseNotFound('not found')
 
-def single_gadget_slug_view(request, gadget_slug):
-    gadget_match = None
-    for gadget in gadgets:
-        if slugify(gadget['name']) == gadget_slug:
-            gadget_match = gadget
-    if gadget_match: 
-        return JsonResponse(gadget_match)
-    raise Http404()
-
-@csrf_exempt
-def single_gadget_post_view(request):
-    if request.method == "POST":
+class GadgetView(View):
+    def get(self, request, gadget_slug=""):
+        gadget_match = None
+        for gadget in gadgets:
+            if slugify(gadget['name']) == gadget_slug:
+                gadget_match = gadget
+        if gadget_match: 
+            return JsonResponse(gadget_match)
+        raise Http404()
+    
+    def post(self, request):
         try:
             data = json.loads(request.body)
             print(f"recived data: {data}")
             return JsonResponse({"response": 'Läuft'})
         except Exception:
             return JsonResponse({"response": 'Läuft nicht'})
-    return HttpResponse('Die verwendete request.method lautet ' + request.method)
+
+def single_gadget_post_view(request):
+    pass
